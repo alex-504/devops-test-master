bf834594191e74fe597625b36f2af0b425990399
alex-504 <alexandre.ealimentos@gmail.com>
2025-07-01 00:11:59 +0900

    Update RDS engine_version to 17.4 (latest available in region) for compatibility

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,6 +1,9 @@
 # Variables for our beer app infrastructure
 # These are like ingredients in a recipe - you can change them easily
 
+# To set the Docker image for ECS, create a terraform.tfvars file with:
+# app_image = "311450700715.dkr.ecr.ap-southeast-2.amazonaws.com/my-ecr-repo:latest"
+
 variable "aws_region" {
   description = "Which AWS region to use (like us-east-1)"
   type        = string
